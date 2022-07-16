0c835c5e35bd69710c35bc470f774588d07a23e7
Steven Feltner <steven.feltner@netapp.com>
2022-07-16 16:16:38 -0700

    add task, schedule, loadbalancer, examples

---
+++ examples/basic.tf
@@ -0,0 +1,21 @@
+## Create Ocean Cluster in Spot.io ##
+module "ocean-aws-k8s" {
+  source  = "spotinst/ocean-aws-k8s/spotinst"
+
+  # Configuration
+  cluster_name                = "EKS-Example"
+  region                      = "us-west-2"
+  subnet_ids                  = ["subnet-12345678","subnet-12345678"]
+  # instance profile arn should have the EKSWorkerNodePolicy attached
+  worker_instance_profile_arn = "arn:aws:iam::123456789:instance-profile/Spot-EKS-Workshop-Nodegroup"
+  security_groups             = ["sg-123456789","sg-123456789"]
+
+  # Additional Tags
+  tags = {CreatedBy = "terraform"}
+}
+
+
+## Outputs ##
+output "ocean_id" {
+  value = module.ocean-aws-k8s.ocean_id
+}
