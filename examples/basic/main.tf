874f30a65c430c2b1c25af99d3c0d5eb304b164c
Sharad Kesarwani <108344822+sharadkesarwani@users.noreply.github.com>
2023-09-08 12:39:55 +0530

    Spotaut 16394 support bdm (#15)
    * Added support for BDM
    
    Added support for BDM
    
    * Updated documentation
    
    Updated documentation
    
    * alignment modification
    
    alignment modification
    
    ---------
    
    Co-authored-by: chandra1-n <86221454+chandra1-n@users.noreply.github.com>

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -12,4 +12,19 @@ module "ocean-aws-k8s" {
 
   # Additional Tags
   tags = {CreatedBy = "terraform"}
+  # Block Device Mappings
+  block_device_mappings       = [{
+    device_name               = "/dev/xvda"
+    delete_on_termination     = true
+    encrypted                 = false
+    kms_key_id                = "alias/aws/ebs"
+    snapshot_id               = null
+    iops                      = 1
+    volume_type               = "gp2"
+    volume_size               = null
+    throughput                = 125 }]
+  dynamic_volume_size         = {
+    base_size                 = 50
+    resource                  = "CPU"
+    size_per_resource_unit    = 20 }
 }
