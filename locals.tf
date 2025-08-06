7a30a60b4d0af3a7847467ac373511f3da58e40a
chandra1-n <86221454+chandra1-n@users.noreply.github.com>
2025-08-06 22:52:35 +0530

    update filter expression
    update filter expression

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  worker_ami_name_filter = "amazon-eks-node-${data.aws_eks_cluster.cluster.version}-v*"
+  worker_ami_name_filter = data.aws_eks_cluster.cluster.version <= "1.32" ? "amazon-eks-node-${data.aws_eks_cluster.cluster.version}-v*" : "amazon-eks-node-*-${data.aws_eks_cluster.cluster.version}-v*"
 }
 
